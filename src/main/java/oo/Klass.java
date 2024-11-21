ecca582a8a9288e9b6a6c02f4afc63f9931d6a6a
WUKE7 <kevin.wu@oocl.com>
2024-11-21 14:08:02 +0800

    feat: Override equals and hashCode function of Klass

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,24 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+    private int id;
+
+    public Klass(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return id == klass.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
